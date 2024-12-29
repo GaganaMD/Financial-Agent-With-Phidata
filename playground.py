6f8ab504f8d091bdab48fb390b8d87a18d2af750
GaganaMD <gagana912dev@gmail.com>
2024-12-29 12:08:44 +0530

    create playground

--- playground.py
+++ playground.py
@@ -1,9 +1,16 @@
+import os
+import phi
+import phi.api
+
+from dotenv import load_dotenv
 from phi.agent import Agent
 from phi.model.groq import Groq
-# from phi.tools.yfinance import YFinance
 from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
+from phi.playground import Playground, serve_playground_app
 
+load_dotenv()
+phi.api = os.getenv("PHI_API_KEY")
 
 # Web search agent
 websearch_agent = Agent(
@@ -28,15 +35,10 @@ financial_agent = Agent(
     markdown=True
 )
 
-# Multi-agent system
+# playground
+app = Playground(
+    agents = [websearch_agent, financial_agent]
+    ).get_app(),
 
-multi_agent_system = Agent(
-    team = [websearch_agent, financial_agent],
-    model = Groq(id="llama3-groq-70b-8192-tool-use-preview"),
-    instructions=["Always include sources","Use tabels to display the data"],
-    show_tool_calls=True,
-    markdown=True
-)
-
-multi_agent_system.print_response("Summarise analyst recommendations and share the latest news for NVDA",stream=True)
-
+if __name__ == "__main__":
+    serve_playground_app("playground:app",reload=True)
